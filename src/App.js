88bf4caed40eacbb7427d8e03dca0dc25ad955a3
kabeer-necrox <khushkabeer@gmail.com>
2023-08-19 12:50:21 +0300

    this is the code of from

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import React,{useState}from 'react'
+
+import Login from './Login';
+
 function App() {
-  const [data,setData]=useState("Anil")
-  return (
-    <div className="App">
-      <h1>Hooks in React {data}</h1>
-      <button onClick={()=>setData("Peter")}>Update State</button>
+ 
+  return(
+ <div>
+
+      <Login />
     </div>
   );
 }
